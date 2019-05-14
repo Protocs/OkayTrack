8972a9cbebebeaec0b4179480937fa78b5e8edee
Protocs <utki.nikita@yandex.ru>
2019-05-14 11:26:25 +0300

    index.html

--- website/app.py
+++ website/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
 from .config import FORM_SECRET_KEY
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = FORM_SECRET_KEY
 app.config["JSON_AS_ASCII"] = False
+Bootstrap(app)
